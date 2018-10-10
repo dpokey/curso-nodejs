cfa8e90e211f68cdd15065a4e548880bb49275fe
4lbertoDelgado <albertodq7@gmail.com>
2018-10-10 17:18:30 -0500

    Update notes.js

--- 1.NODEJS_FUNDAMENTALS/notes-node/notes.js
+++ 1.NODEJS_FUNDAMENTALS/notes-node/notes.js
@@ -1,8 +1,12 @@
 console.log('Iniciando notes.js..');
 
-// module.exports.age = 25;
+module.exports.age = 25;
 
 module.exports.addNotes = () => {
     console.log('addNotes');
     return 'New Note';
+}
+
+module.exports.add = (num1, num2) => {
+    return num1 + num2;
 }
