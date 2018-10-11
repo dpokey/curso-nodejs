aa2ff9bda07e077665262ee5e2d89436ffffb6c8
4lbertoDelgado <albertodq7@gmail.com>
2018-10-11 18:31:37 -0500

    Update notes.js

--- 1.NODEJS_FUNDAMENTALS/notes-node/notes.js
+++ 1.NODEJS_FUNDAMENTALS/notes-node/notes.js
@@ -1,12 +1,39 @@
 console.log('Iniciando notes.js..');
 
-module.exports.age = 25;
+//  declaramos las funciones  
+var addNote = (title, body) => {
+    console.log('addNote', title, body);
+};
+
+var getAll = () => {
+    console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+    console.log('Reading note', title);
+};
+
+var removeNote = (title) => {
+    console.log('Removing note', title);
+}
+
+// y las exportamos. en ES6 cuando nombrePropiedad = nombreValor se coloca directamoente solo nombrePropiedad
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+};
+
+/*  -------------------------------------------  */
 
 module.exports.addNotes = () => {
     console.log('addNotes');
     return 'New Note';
-}
+};
 
 module.exports.add = (num1, num2) => {
     return num1 + num2;
-}
+};
+
+
