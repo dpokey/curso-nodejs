ec47fa83a2b5cc6e0ee373cd0bd78ab289a0012f
4lbertoDelgado <albertodq7@gmail.com>
2018-10-17 17:15:26 -0500

    Add files via upload

---
+++ 2.NODEJS_ASYNCHRONOUS/node.js
@@ -0,0 +1,4 @@
+/*  ---- Programacion Asincrona JS ----  */
+/* 
+    Node al ser asyncrono, permite ejecutar codigo sin necesidad de esperar por la devolucion de otras llamas, puede ir ejecutando miestras espera.
+*/
