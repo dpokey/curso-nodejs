42ca59396eb2eefc95c10c48498689d925247dec
4lbertoDelgado <albertodq7@gmail.com>
2018-10-19 17:31:53 -0500

    Update node.js

--- 2.NODEJS_ASYNCHRONOUS/node.js
+++ 2.NODEJS_ASYNCHRONOUS/node.js
@@ -1,4 +1,16 @@
 /*  ---- Programacion Asincrona JS ----  */
 /* 
-    Node al ser asyncrono, permite ejecutar codigo sin necesidad de esperar por la devolucion de otras llamas, puede ir ejecutando miestras espera.
+    Node al ser asyncrono, permite ejecutar codigo sin necesidad de esperar por la devolucion de otras llamas,  
+    puede ir ejecutando miestras espera.
+
+    Call Stack
+    ----------
+    Es una estructura de datos muy simple que realiza un seguimiento de la ejecucion del programa dentro de la 
+    Node APIs
+    ---------
+
+    Callback Queue
+    --------------
+
+
 */
