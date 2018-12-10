b3f28222025b279c0bfda9420f1908ac86fcc63f
4lbertoDelgado <albertodq7@gmail.com>
2018-12-10 22:27:58 +0000

    Update async-basics.js

--- 2.NODEJS_ASYNCHRONOUS/weather-app/playground/async-basics.js
+++ 2.NODEJS_ASYNCHRONOUS/weather-app/playground/async-basics.js
@@ -1,10 +1,18 @@
+
+/*  Al ejecutar este programa podemos observar el funcionamiento de un codigo asincrono donde 
+    nodejs no espera los 2 segundos, sino que registra la peticion de llamada pero sigue ejecutando las
+    demas lines de codigo */
 console.log('Starting app')
 
 setTimeout(() => {
     console.log('Inside of callback')
 },2000)
 
+/* aqui existe un comportamiento extraño, ya que deberia ejecutarse en 0 milisegundos, pero se ejecuta
+   esto sucede debido a que detras ocuarren cosas como el Call Stack, Node APIs, Callback Queue, Event Loop */
+   
 setTimeout(() => {
     console.log('Second setTimeout')
 },0)
+
 console.log('Finishing app')
